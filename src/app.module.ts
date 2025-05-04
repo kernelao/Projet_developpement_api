dffbb6e562fc0e7aeb9ede2e93ca6bdaed551e49
kernelao <oubalkass.ayoub@hotmail.com>
2025-05-04 18:23:40 -0400

    Connexion à la base de donnée PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'symfony',
+      password: 'secret',
+      database: 'monapi',
+      synchronize: true, // à mettre false en prod
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
