5716bc646dc8d0a4434b183aaf110a218d8c1943
mirai12d <TonEmailGitHub>
2025-05-06 02:24:42 -0400

    Supprimer dist/ du suivi Git

--- dist/auth/register.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class RegisterDto {
-    username: string;
-    nom: string;
-    prenom: string;
-    email: string;
-    password: string;
-    telephone: string;
-    entreprise?: string;
-}
--- dist/user/user.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
-export declare class UserService {
-    private repo;
-    constructor(repo: Repository<User>);
-    create(user: Partial<User>): Promise<User>;
-    findByEmail(email: string): Promise<User | null>;
-    findByUsername(username: string): Promise<User | null>;
-}
